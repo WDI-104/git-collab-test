6202276ab8333b8310fec8ff70f0fa7a88cc2882
Juan <juan.giana8589@gmail.com>
2023-07-20 13:21:53 -0500

    add FeatureJuan

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import DefaultComponent from './components/DefaultComponent'
 import Feature1 from './components/Feature1'
 import Feature2 from './components/Feature2'
 import Feature3 from './components/Feature3'
+import FeatureJuan from './components/FeatureJuan'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Feature1 />
       <Feature2 />
       <Feature3 />
+      <FeatureJuan />
     </>
   )
 }
