03f3ae76530ed797a6b1baeed33c5ef8c4042d7e
glf30 <greyson.frazier@gmail.com>
2023-07-20 11:30:25 -0400

    Add feature component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DefaultComponent from './components/DefaultComponent'
+import Feature1 from './components/Feature1'
 
 function App() {
 
   return (
     <>
       <DefaultComponent />
+      <Feature1 />
     </>
   )
 }
