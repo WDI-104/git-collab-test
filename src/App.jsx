9afd305e7d1374846600902e8bc34741cacb8d8b
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-07-20 14:20:54 -0400

    addTerrancecommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import DefaultComponent from './components/DefaultComponent'
-import Feature1 from './components/Feature1'
-import Feature2 from './components/Feature2'
-import Feature3 from './components/Feature3'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import DefaultComponent from "./components/DefaultComponent";
+import Feature1 from "./components/Feature1";
+import Feature2 from "./components/Feature2";
+import Feature3 from "./components/Feature3";
+import FeatureTerranceRivers from "./components/Feature-TerranceRivers";
 
 function App() {
-
   return (
     <>
       <DefaultComponent />
       <Feature1 />
       <Feature2 />
       <Feature3 />
+      <FeatureTerranceRivers />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
